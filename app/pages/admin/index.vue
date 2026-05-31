3dd853cd6529b7c76202206961f32305c7fd2b8d
RezaPouya <rpouya@hotmail.com>
2026-05-31 21:44:36 +0330

    درست کردن عنوان صفحه

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -3,7 +3,8 @@ import * as z from 'zod'
 import type { FormSubmitEvent } from '@nuxt/ui'
 definePageMeta({
   layout: 'admin',
-  middleware: 'admin'
+  middleware: 'admin',
+  title: 'داشبورد مدیریت'
 })
 </script>
 
